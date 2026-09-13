4b39f4a6fc837ea73abbb5c0e531e6be71c364e5
Muhamad Faris Abdillah <muhamad.farisabdillah@gmail.com>
2026-09-13 19:19:12 +0700

    Tambah lokasi, produk dan ubah tombol promo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+  promoButton.textContent = "Promo Anda sudah aktif! Silahkan tunjukan ke kasir.";
+  console.log("Promo Kopi berhasil diklaim pengguna.");
 });
